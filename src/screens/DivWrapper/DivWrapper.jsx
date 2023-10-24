6ecad3c68601c20bca02ee248136612296429814
HYEHWA <116864528+hyehwa615@users.noreply.github.com>
2023-10-25 00:21:44 +0900

    Add files via upload

---
+++ src/screens/DivWrapper/DivWrapper.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import "./style.css";
+
+export const DivWrapper = () => {
+  return (
+    <div className="div-wrapper">
+      <div className="div">
+        <div className="overlap">
+          <img className="image" alt="Image" src="/img/image-124.png" />
+          <div className="group">
+            <div className="overlap-group">
+              <div className="text-wrapper">InPrism</div>
+              <div className="text-wrapper-2">InPrism</div>
+            </div>
+            <p className="p">
+              <span className="span">솔직한</span>
+              <span className="text-wrapper-3">&nbsp;</span>
+              <span className="text-wrapper-4">감정</span>
+              <span className="span">의</span>
+              <span className="text-wrapper-3">&nbsp;</span>
+              <span className="text-wrapper-4">형태</span>
+              <span className="span">를 마주해 보세요. </span>
+            </p>
+          </div>
+          <div className="rectangle" />
+          <img className="vector" alt="Vector" src="/img/vector-15.png" />
+          <div className="overlap-wrapper">
+            <div className="overlap-2">
+              <img
+                className="medium-shot-friends"
+                alt="Medium shot friends"
+                src="/img/medium-shot-friends-holding-each-other-1.png"
+              />
+              <img className="img" alt="Rectangle" src="/img/rectangle-112.png" />
+            </div>
+          </div>
+          <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-118.png" />
+          <div className="view">
+            <div className="text-wrapper-5">Overview</div>
+            <p className="div-2">
+              <span className="text-wrapper-6">왜 우리는 </span>
+              <span className="text-wrapper-7">감정표현</span>
+              <span className="text-wrapper-6">을 어려워할까?</span>
+            </p>
+            <div className="flexcontainer">
+              <p className="text">
+                <span className="text-wrapper-8">
+                  이 물음은 평범한 일상 속, 너무나 자연스럽게 자신의 감정을 감추는 모습에서 시작되었습니다.
+                  <br />
+                </span>
+              </p>
+              <p className="text">
+                <span className="text-wrapper-8">
+                  현대 사회에서는 모두가 연결되어 있음에도 불구하고, 서로의 진정한 감정을 공유하기 어려운 환경 속에서
+                  살아갑니다. <br />
+                </span>
+              </p>
+              <p className="text">
+                <span className="text-wrapper-9">
+                  인프리즘은 이러한 감정을 더욱 흥미로운 환경 속에서 드러낼 수 있도록 도와주는 인터랙티브 미디어월
+                  콘텐츠입니다.
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="group-2">
+            <div className="text-wrapper-10">거부에 대한 예기불안</div>
+            <div className="text-wrapper-11">부정적인 감정 억제</div>
+            <div className="text-wrapper-12">감정 회피</div>
+            <div className="text-wrapper-13">감정적 대응</div>
+            <div className="text-wrapper-14">부적응적 감정조절</div>
+            <div className="text-wrapper-15">감정억제로 인한 조절 어려움</div>
+          </div>
+          <div className="view-2">
+            <p className="text-wrapper-16">
+              자신의 감정을 표현하고자 하는 욕구가 있음에도 불구하고, 이를 억제하거나 통제할 때 경험하는 갈등을
+              의미합니다.
+            </p>
+            <p className="text-wrapper-17">정서표현 양가성 높을수록 감정 표현 어려움</p>
+            <div className="text-wrapper-18">Background</div>
+            <img className="group-3" alt="Group" src="/img/group-75.png" />
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-3">
+              <div className="rectangle-3" />
+              <div className="ellipse" />
+            </div>
+          </div>
+          <div className="group-4">
+            <p className="text-wrapper-19">감정에 대한 어려움을 해소시켜, 표현할 수 있는 첫걸음 제공합니다.</p>
+            <div className="text-wrapper-20">Solution</div>
+            <p className="text-wrapper-21">다양한 감정의 형태 체험 콘텐츠</p>
+          </div>
+          <img className="kakaotalk-photo" alt="Kakaotalk photo" src="/img/kakaotalk-photo-2023-10-24-17-25-06-1.png" />
+        </div>
+        <p className="text-wrapper-22">5개의 감정을 표현한 오브제과 공간을 제작했습니다.</p>
+        <p className="text-wrapper-23">공간 배치와 실루엣 인터랙션을 제작했습니다.</p>
+        <div className="text-wrapper-24">작업과정 및 시연영상</div>
+        <div className="text-wrapper-25">팀을 소개합니다!</div>
+        <div className="overlap-4">
+          <div className="text-wrapper-26">감정 공간</div>
+          <div className="ellipse-2" />
+        </div>
+        <div className="overlap-5">
+          <div className="text-wrapper-27">감정 오브제</div>
+          <div className="ellipse-2" />
+        </div>
+        <div className="overlap-6">
+          <div className="text-wrapper-27">감정 실루엣</div>
+          <div className="ellipse-2" />
+        </div>
+        <div className="text-wrapper-28">3D Modeling</div>
+        <div className="text-wrapper-29">Unity Kinect</div>
+        <div className="text-wrapper-30">Video</div>
+        <div className="text-wrapper-31">Team</div>
+        <div className="group-5">
+          <img className="kakaotalk" alt="Kakaotalk" src="/img/kakaotalk-20230925-203513775-1.png" />
+          <img className="mask-group" alt="Mask group" src="/img/mask-group-2.png" />
+          <img className="mask-group-2" alt="Mask group" src="/img/mask-group-1.png" />
+          <img className="mask-group-3" alt="Mask group" src="/img/mask-group.png" />
+        </div>
+        <div className="text-wrapper-32">#분노</div>
+        <div className="text-wrapper-33">#공허</div>
+        <div className="text-wrapper-34">#행복</div>
+        <div className="text-wrapper-35">#슬픔</div>
+        <div className="text-wrapper-36">#공포</div>
+        <div className="rectangle-4" />
+        <div className="overlap-7">
+          <div className="group-6">
+            <div className="overlap-8">
+              <img className="kakaotalk-2" alt="Kakaotalk" src="/img/kakaotalk-20231010-140311226.png" />
+              <img className="kakaotalk-3" alt="Kakaotalk" src="/img/kakaotalk-20231015-071015908-1.png" />
+            </div>
+            <img className="kakaotalk-4" alt="Kakaotalk" src="/img/kakaotalk-20231010-140625243.png" />
+          </div>
+          <div className="rectangle-5" />
+          <div className="text-wrapper-37">김이설</div>
+          <div className="text-wrapper-38">추혜화</div>
+          <div className="text-wrapper-39">강희영</div>
+          <div className="text-wrapper-40">고주현</div>
+          <div className="text-wrapper-41">오민아</div>
+          <div className="text-wrapper-42">Programmer</div>
+          <div className="text-wrapper-43">(팀장)</div>
+          <div className="text-wrapper-44">Publisher</div>
+          <div className="text-wrapper-45">Planner/Designer</div>
+          <div className="text-wrapper-46">3D Designer</div>
+          <div className="text-wrapper-47">3D Designer</div>
+          <img className="element" alt="Element" src="/img/1-2.png" />
+          <img className="element-2" alt="Element" src="/img/1-1.png" />
+        </div>
+        <img className="image-2" alt="Image" src="/img/image-103.png" />
+        <img className="image-3" alt="Image" src="/img/image-104.png" />
+        <img className="image-4" alt="Image" src="/img/image-105.png" />
+        <img className="image-5" alt="Image" src="/img/1-2x.png" />
+        <img className="image-6" alt="Image" src="/img/2.png" />
+        <img className="image-7" alt="Image" src="/img/image.png" />
+        <img className="image-8" alt="Image" src="/img/1.png" />
+        <img className="image-9" alt="Image" src="/img/2x.png" />
+      </div>
+    </div>
+  );
+};
